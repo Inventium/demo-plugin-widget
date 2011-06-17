7361a362b8f9e3a668113a7c5c0403798faf0784
Dave Doolin <david.doolin@gmail.com>
2011-06-17 08:53:56 -0700

    Refactored form field handling.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # WordPress demo plugin for demonstrating widget
 
-Refer to the WordPress "readme.txt" file for details.
+Widgets are pretty cool, and give a WordPress operator 
+a lot of power to extend functionality.
+
+This demo plugin provides a very basic set of widget
+functions, which can be easily extended.
+
+Note that the form field are handled in instance methods
+for the widget class. In the future output fields should 
+handled in the same way.
 
--- demo-plugin-widget.php
+++ demo-plugin-widget.php
@@ -12,6 +12,7 @@
 if (!class_exists("demo_plugin_widget")) {
 
   class demo_plugin_widget extends WP_Widget {
+
       
     var $css_class = 'demo_plugin_widget_class';
     var $description = 'A minimal WordPress widget defined using a short, simple plugin';
@@ -20,21 +21,28 @@ if (!class_exists("demo_plugin_widget")) {
     var $default_title = 'Cool demo widget plugin';
     var $default_demotext = 'Some default demo text for just for fun.';
 
+
     function demo_plugin_widget() {
+      
       $widget_ops = array('classname' => $this->css_class, 
                           'description' => $this->description);
       $this->WP_Widget($this->base_id, $this->name, $widget_ops);
     }
 
+
     /* This is the code that gets displayed on the UI side,
      * what readers see.
      */
     function widget($args, $instance) {
+      
       extract($args, EXTR_SKIP);
       echo $before_widget;
       $title = (empty($instance['title'])) ? $this->default_title : apply_filters('widget_title', $instance['title']); 
       $demotext = (empty($instance['demotext'])) ? $this->default_demotext : $instance['demotext']; 
       echo $before_title . $title . $after_title; 
+      /**
+       * TODO: Refactor the output into it's own function(s)...
+       */
 ?>
 <ul>
   <li><?php echo $demotext ?></li>
@@ -44,38 +52,60 @@ if (!class_exists("demo_plugin_widget")) {
       echo $after_widget;
     }
 
+
     function update($new_instance, $old_instance) {
+      
       $instance = $old_instance;
       $instance['title'] = strip_tags($new_instance['title']);
       $instance['demotext'] = strip_tags($new_instance['demotext']);
       return $instance;
     }
 
+
     /* Back end, the interface shown in Appearance -> Widgets
      * administration interface.
      */
     function form($instance) {
+      
       $instance = wp_parse_args( (array) $instance, array( 'title' => '', 'entry_title' => '', 'comments_title' => '', 'demotext' => '' ) );
-      $title    = strip_tags($instance['title']);
-      $demotext = strip_tags($instance['demotext']);
-      ?>
 
-<p>
-<label for="<?php echo $this->get_field_id('title'); ?>">Title: 
-    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
-     name="<?php echo $this->get_field_name('title'); ?>" type="text"
-     value="<?php echo esc_attr($title); ?>" />
-</label>
-<label for="<?php echo $this->get_field_id('demotext'); ?>">Demo text: 
-    <input class="widefat" id="<?php echo $this->get_field_id('demotext'); ?>"
-     name="<?php echo $this->get_field_name('demotext'); ?>" type="text"
-     value="<?php echo esc_attr($demotext); ?>" />
-</label>
-</p>
+      $title    = esc_attr(strip_tags($instance['title']));
+      $title_id = $this->get_field_id('title');
+      $title_name = $this->get_field_name('title');
 
-      <?php
-    }      
-  }
+      $demotext = esc_attr(strip_tags($instance['demotext']));
+ 
+      echo "<p>";
+      echo $this->text_input_instance($instance, 'title', 'Title');
+      echo $this->text_input_instance($instance, 'demotext', 'Demo text');
+      echo "</p>";
+     }
+ 
+
+    // Clean everything up...
+    function text_input_instance($instance, $key, $label) {
+
+      $value    = esc_attr(strip_tags($instance[$key]));    
+      $id = $this->get_field_id($key);
+      $name = $this->get_field_name($key);
+      return $this->text_input($id, $name, $value, $label);
+    }
+
+
+    // This is where overloading would be real handy...
+    function text_input($id, $name, $value, $label) {
+    
+      $input = <<<EOI
+      <label for="$id">$label: 
+        <input class="widefat" id="$id" name="$name" type="text" value="$value" />
+      </label>
+EOI;
+      return $input;
+    }
+
+  } // Close class definition...
+
+
 
   function demo_widget_init() {
     // http://codex.wordpress.org/Function_Reference/register_widget
@@ -83,9 +113,9 @@ if (!class_exists("demo_plugin_widget")) {
   }
   add_action('widgets_init', 'demo_widget_init');
 
-}
+} // class_exists()...
 
 // Uncomment this for standalone widget testing... 
-$wpdpd = new demo_plugin_widget();
+//$wpdpd = new demo_plugin_widget();
 
 ?>
